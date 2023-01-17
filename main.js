13e6e860fc335246c955095ed594dcfffdda7b58
vasantichavda <vasantivaghela2812@gmail.com>
2023-01-17 17:14:55 +0530

    Higher order functions

--- main.js
+++ main.js
@@ -1,2 +1,82 @@
-const heading = document.querySelector("h1");
-heading.textContent = "Hello World !";
+const companies= [
+  {name: "Company One", category: "Finance", start: 1981, end: 2004},
+  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+  {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+//forEach
+// companies.forEach(function(company) {
+//   console.log(company);
+// });
+
+// ------------------------------------------------------------
+
+// filter   ---- it filter out it does not create the new array.
+// const canDrink = ages.filter(function(age) {
+//   if(age >= 21) {
+//     return true;
+//   }
+// })
+
+// console.log(canDrink);
+
+
+// ----------------------------------------------------------------
+//filter
+// const eightiesCompanies = companies.filter(function(company) {
+//   if(company.start >= 1980 && company.start < 1990) {
+//     return company;
+//   }
+// })
+
+// console.log(eightiesCompanies);
+
+
+// ---------------------------------------------------------------
+
+//map ---------------- it creates new array
+// const companyNames = companies.map(function(company) {
+//   return company.name ;
+// })
+
+// console.log(companyNames);
+
+//map
+// const testMap = companies.map(function(company) {
+//   return `${company.name} [${company.start} - ${company.end}] `;
+// })
+
+// console.log(testMap);
+
+// --------------------------------------------------------------
+//sort
+// const sortAgeLowToHigh = ages.sort(function(a,b) {
+//   return a-b;
+// })
+
+// console.log(sortAgeLowToHigh);
+
+//sort 
+// const sortedCompanies = companies.sort(function(a,b) {
+//    return a.start > b.start ? 1 : -1 ;
+// })
+
+// console.log(sortedCompanies);
+
+// ----------------------------------------------------------------
+
+
+//reduce
+const ageSum = ages.reduce(function(total, age) {
+  return total + age;
+}, 0);
+
+console.log(ageSum);
