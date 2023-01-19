cf3e850dcd46dd68e73f43f55d3b34a7ddcf805c
vasantichavda <vasantivaghela2812@gmail.com>
2023-01-19 16:06:11 +0530

    post request is examined

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 // const post = document.querySelector('div');
 const button = document.querySelector('button');
+const form = document.getElementById('addPost');
+
 
 
 // function reqListener() {
@@ -30,6 +32,26 @@ function loadPosts() {
     .catch(err => {
       console.log('failed');
     })
+  }
+
+  form.addEventListener('submit', addPost);
+
+  function addPost(e) {
+     e.preventDefault();
+
+     let title = document.getElementById('title').value;
+     let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain , */*',
+        'Content-type': 'application/json'
+      },
+      body:JSON.stringify({title,body})
+     })
+     .then(res => res.json())
+     .then(data => console.log(data))
   }
   
 
