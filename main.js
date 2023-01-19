29e60ccc6244f1c08580fffa0fee38b09e01befa
vasantichavda <vasantivaghela2812@gmail.com>
2023-01-19 12:16:07 +0530

    Understand local , session and cookies

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-const heading = document.querySelector("h1");
-heading.textContent = "Hello World !";
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+
+h1.innerHTML = localStorage.getItem('value');
+input.addEventListener("keyup", display);
+
+function display (){
+  // h1.innerHTML = input.value;
+  localStorage.setItem('value', input.value);
+  h1.innerHTML = localStorage.getItem('value');
+  
+}
+//  localStorage.clear()  //clear all the data in local storage
